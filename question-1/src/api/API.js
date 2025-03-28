e0e575f5505dae9763189c09fb684c1de9a0743c
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2025-03-28 15:03:58 +0530

    post , user api

--- question-1/src/api/API.js
+++ question-1/src/api/API.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:3001"; 
 
-export const fetchUSers = async() => {
-    const response = await axios.get(`${API_URL}/users`);
+export const fetchUsers = async () => {
+    const response = await axios.get(`${API_URL}/users?_sort=postCount&_order=desc`);
     return response.data;
 };
 
-export const fetchPosts = async() => {
-    const response = await axios.get(`${API_URL}/posts`);
+export const fetchPosts = async () => {
+    const response = await axios.get(`${API_URL}/posts?_sort=timestamp&_order=desc`);
     return response.data;
+};
+
+export const fetchTrendingPosts = async () => {
+    const response = await axios.get(`${API_URL}/posts`);
+    const posts = response.data;
+    const maxComments = Math.max(...posts.map(post => post.commentCount));
+    return posts.filter(post => post.commentCount === maxComments);
 };
