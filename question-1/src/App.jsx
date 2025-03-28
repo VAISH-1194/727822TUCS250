09b39e28b0c113cd88f561584674435282d3af01
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2025-03-28 15:04:05 +0530

    route

--- question-1/src/App.jsx
+++ question-1/src/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-import List from "./components/List";
+// import React from "react";
+// import List from "./components/List";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+//       <List />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import TopUsers from './pages/TopUsers';
+import Trending from './pages/Trending';
+import Feed from './pages/Feed';
+
+export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <List />
+    <div className="min-h-screen bg-gray-100">
+      <Nav />
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<TopUsers />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      </div>
     </div>
   );
-};
-
-export default App;
+}
