701d37a55cb9ae3287f46e1f50745c48b3015281
VAISHNAVI M <vaishnavimadhavan1194@gmail.com>
2025-03-28 14:04:31 +0530

    user list display

--- question-1/src/App.jsx
+++ question-1/src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import User from "./components/User";
+import List from "./components/List";
 
 const App = () => {
-  const testUser = {
-    name: "Anonymouos",
-    posts: ["Post-1", "Post-2", "Post-3"],
-  };
-
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">User Cards</h1>
-      <User user={testUser} />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <List />
     </div>
   );
 };
---
+++ question-1/src/components/List.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import User from "./User";
+
+const List = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        setUsers(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError("Error in fetching users :( ");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p className="text-center">Loading users...</p>;
+  if (error) return <p className="text-center text-red-500">{error}</p>;
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4 text-center">User List</h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        {users.map(user => (
+          <User key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default List;
